302a418633ea2a327319bbe54363b7a0bd6c3c1b
SallaPR <salla.pr@hotmail.com>
2025-03-18 22:33:05 +0200

    edited welcome screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button, TouchableOpacity} from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button, TouchableOpacity, Image } from "react-native";
+import { Colors } from "../theme/colors";
 
 const WelcomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Tervetuloa Appiin</Text>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')} >
-      <Text style={styles.buttonText}>Kirjaudu</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Register')}>
-      <Text style={styles.buttonText}>Rekisteröidy</Text>
-      </TouchableOpacity>     
+      <Image
+        source={require("../assets/background.jpg")}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>Tervetuloa Appiin</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Login")}
+        >
+          <Text style={styles.buttonText}>Kirjaudu</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Register")}
+        >
+          <Text style={styles.buttonText}>Rekisteröidy</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -18,28 +32,45 @@ const WelcomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor:"#006A66"
+    alignItems: "center",
+    backgroundColor: Colors.onPrimaryFixed,
   },
-  text: {
-    fontSize: 20,
+  image: {
+    width: "100%",
+    height: "55%",
+  },
+  contentContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: "50%",
+    backgroundColor: Colors.onPrimaryFixed,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 50,
     marginBottom: 20,
-    color:"white"
+    color: "white",
+    textAlign: "center",
+    fontFamily: "Exo",
   },
   button: {
     width: 190,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingVertical: 10,
     paddingHorizontal: 40,
     borderRadius: 40,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
+    marginBottom: 15,
   },
   buttonText: {
-    color: 'black', 
+    color: "black",
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
 
