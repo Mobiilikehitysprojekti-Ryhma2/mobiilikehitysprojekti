fe0089ecf35fbe0be04381e1a0378f077fee496e
SallaPR <salla.pr@hotmail.com>
2025-03-19 14:46:49 +0200

    loginscreen styling done

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,84 +1,157 @@
-import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity} from 'react-native';
-import { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import { Colors } from "../theme/colors";
+import { useFonts, Exo_400Regular } from "@expo-google-fonts/exo";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const LoginScreen = ({ navigation }) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleLogin = () => {
-        console.log("Email:", email);
-        console.log("Password:", password);
-      };
+  let [fontsLoaded, fontError] = useFonts({ Exo_400Regular });
+
+  if (!fontsLoaded && fontError) {
+    return null;
+  }
+
+  const handleLogin = () => {
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Kirjaudu sisään</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Sähköposti"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Salasana"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <TouchableOpacity style={styles.button} onPress={handleLogin} >
-        <Text style={styles.buttonText}>Kirjaudu</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')} >
-        <Text style={styles.buttonText}>Jatka</Text>
-      </TouchableOpacity>
-      <Text style={styles.footer}>Eikö sinulla ole vielä tiliä?</Text>
-      <Button title="Rekisteröidy" onPress={() => navigation.navigate('Register')} />
+      <View style={styles.topBackground}>
+        <Text style={styles.title}>Kirjaudu sisään</Text>
+      </View>
+
+      <View style={styles.bottomBackground} />
+
+      <View style={styles.contentContainer}>
+        <View style={styles.inputContainer}>
+          <MaterialIcons name="email" size={20} color={Colors.secondary} />
+          <TextInput
+            style={styles.input}
+            placeholder="Sähköposti"
+            placeholderTextColor={Colors.secondary}
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <MaterialIcons name="lock" size={20} color={Colors.secondary} />
+          <TextInput
+            style={styles.input}
+            placeholder="Salasana"
+            placeholderTextColor={Colors.secondary}
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+        </View>
+
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.loginButtonText}>Kirjaudu</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+          <Text style={styles.loginButtonText}>Jatka</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.registerText}>Eikö sinulla ole vielä tiliä?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+          <Text style={styles.registerLink}>Rekisteröidy</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 20,
-      backgroundColor:"#006A66"
-    },
-    header: {
-      fontSize: 24,
-      marginBottom: 20,
-      color:"white"
-    },
-    input: {
-      width: "100%",
-      padding: 10,
-      marginVertical: 10,
-      borderWidth: 1,
-      borderColor: "#ccc",
-      borderRadius: 5,
-      backgroundColor:"white"
-    },
-    footer: {
-      marginTop: 20,
-      color: "blue",
-    },
-    button: {
-        width: 190,
-        backgroundColor: '#fff',
-        paddingVertical: 10,
-        paddingHorizontal: 40,
-        borderRadius: 40,
-        borderWidth: 1,
-        borderColor: '#ccc',
-      },
-      buttonText: {
-        color: 'black', 
-        fontSize: 16,
-        textAlign: 'center',
-      },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+  topBackground: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.onPrimaryFixed,
+  },
+  bottomBackground: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    alignItems: "center",
+  },
+  contentContainer: {
+    position: "absolute",
+    top: "40%",
+    alignSelf: "center",
+    alignItems: "center",
+    width: "90%",
+    backgroundColor: Colors.onPrimary,
+    padding: 20,
+    borderRadius: 20,
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  title: {
+    fontSize: 40,
+    color: "white",
+    width: "70%",
+    textAlign: "center",
+    fontFamily: "Exo_400Regular",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.secondaryContainer,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginTop: 20,
+    marginBottom: 5,
+  },
+  input: {
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 16,
+    color: Colors.secondary,
+  },
+  loginButton: {
+    width: 190,
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 40,
+    borderWidth: 0,
+    backgroundColor: Colors.onPrimaryFixed,
+    margin: 20,
+  },
+  loginButtonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  registerText: {
+    marginTop: 50,
+    color: "#333",
+    fontSize: 14,
+  },
+  registerLink: {
+    fontWeight: "bold",
+    color: "#0F5D52",
+    textDecorationLine: "underline",
+  },
+});
 
 export default LoginScreen;
