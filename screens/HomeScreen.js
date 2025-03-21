4214edb43e2068fe3f11d2d9648fa836b37d0f8d
SallaPR <salla.pr@hotmail.com>
2025-03-21 13:45:39 +0200

    updates

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,42 +1,43 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
 
-export default function HomeScreen() {
-    const [search, setSearch] = useState('');
+export default function HomeScreen({ navigation }) {
+  const [search, setSearch] = useState("");
 
-    const handleSearchChange = (text) => {
-        setSearch(text);
-      };
+  const handleSearchChange = (text) => {
+    setSearch(text);
+  };
 
-  return ( <View style={styles.container}>
-    <TextInput
-      style={styles.searchBar}
-      placeholder="Hae..."
-      value={search}
-      onChangeText={handleSearchChange}
-    />
-    
-    <Text style={styles.content}>Hakutulokset</Text>
-  </View>
-);
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Hae..."
+        value={search}
+        onChangeText={handleSearchChange}
+      />
+
+      <Text style={styles.content}>Hakutulokset</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  padding: 20,
-  backgroundColor: '#006A66',
-},
-searchBar: {
-  height: 40,
-  borderColor: '#ccc',
-  borderWidth: 1,
-  borderRadius: 8,
-  paddingLeft: 10,
-  marginBottom: 20,
-  backgroundColor:"white"
-},
-content: {
-  fontSize: 18,
-},
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#006A66",
+  },
+  searchBar: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingLeft: 10,
+    marginBottom: 20,
+    backgroundColor: "white",
+  },
+  content: {
+    fontSize: 18,
+  },
 });
