a7b9a2e51c0ef0cd37a9c1904ecda32afdeea5ae
SallaPR <salla.pr@hotmail.com>
2025-03-21 11:54:47 +0200

    config file for firebase

---
+++ firebase/Config.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  query,
+  onSnapshot,
+  orderBy,
+} from "firebase/firestore";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import Constants from 'expo-constants';
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.firebaseApiKey,
+  authDomain: Constants.expoConfig.extra.firebaseAuthDomain,
+  projectId: Constants.expoConfig.extra.firebaseProjectId,
+  storageBucket: Constants.expoConfig.extra.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig.extra.firebaseMessagingSenderId,
+  appId: Constants.expoConfig.extra.firebaseAppId,
+};
+
+initializeApp(firebaseConfig);
+
+const firestore = getFirestore();
+
+export {
+  firestore,
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  query,
+  onSnapshot,
+  orderBy,
+  getAuth, 
+  signInWithEmailAndPassword,
+  onAuthStateChanged, 
+  createUserWithEmailAndPassword, 
+  signOut
+};
