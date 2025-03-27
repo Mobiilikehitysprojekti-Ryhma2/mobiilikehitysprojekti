c2ba4aa8dbb175b24f842dccc6bdddc90c6daafa
LasseTrs <156923845+LasseTrs@users.noreply.github.com>
2025-03-27 13:41:36 +0200

    Reititystä ja markkereita
    Signed-off-by: LasseTrs <156923845+LasseTrs@users.noreply.github.com>

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,23 +3,28 @@ import { View, Text, TextInput, StyleSheet, Image, Button, SafeAreaView } from "
 import { useEffect, useRef } from 'react';
 import MapView, { Camera } from 'react-native-maps';
 import * as Location from 'expo-location';
-import {Marker} from 'react-native-maps';
+import {Marker, Polyline} from 'react-native-maps';
 import { FAB } from 'react-native-paper';
+import MapViewDirections from 'react-native-maps-directions';
 
-export default function HomeScreen() {
-    const [search, setSearch] = useState('');
 
-    const handleSearchChange = (text) => {
-        setSearch(text);
-      };
 
-      const [camera, setCamera] = useState('')
-      const [location, setLocation] = useState({
-          latitude: 65.0100,
-          longitude: 25.4719,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        })
+
+export default function HomeScreen({ navigation }) {
+  const [search, setSearch] = useState("");
+  const [camera, setCamera] = useState('')
+  const [markers, setMarkers] = useState([]);
+  //const origin = {latitude: 65.03439, longitude: 25.2803};
+ // const destination = {latitude: 65.0345, longitude: 25.2851};
+ const [showAppOptions, setShowAppOptions] = useState(false);
+ const [isRoutefinderModalVisible, setIsRoutefinderModalVisible] = useState(false);
+
+  const [location, setLocation] = useState({
+      latitude: 65.0100,
+      longitude: 25.4719,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    })
         
         useEffect(() => {
         (async() =>{
@@ -28,6 +33,16 @@ export default function HomeScreen() {
           
         }, [])
   
+      const handleSearchChange = (text) => {
+        setSearch(text);
+      };
+  
+
+
+
+
+
+
       const getUserPosition = async () => {
           let { status } = await Location.requestForegroundPermissionsAsync();
           try {
@@ -48,7 +63,7 @@ export default function HomeScreen() {
   ...camera,
   pitch: 90,
   heading: 0,
-            zoom: 20,
+            zoom: 15,
   
             })
    
@@ -58,10 +73,41 @@ export default function HomeScreen() {
         };
 
 
+        const handleLongPress = (e) => {
+          const coordinate = e.nativeEvent.coordinate;
+          setMarkers([
+          ...markers,
+          {
+          key: markers.length + 1,
+          coordinate: coordinate
+          }
+          ]);
+        };
+
+        const onReset = () => {
+          setMarkers([]);
 
 
+        };
+      
+        const Routefinder = () => {
+          const coordinate = {latitude: 65.06254, longitude: 25.46997};
+          setMarkers([
+          ...markers,
+          {
+          key: markers.length + 1,
+          coordinate: coordinate
+          }
+          ]);
+        };
+      
+        const openRoutefinderModal = () => {
+          setIsRoutefinderModalVisible(true);
+        };
 
-
+        const toggleAppOptions = () => {
+          setShowAppOptions(prevState => !prevState);
+        };
 
 
 
@@ -72,10 +118,10 @@ export default function HomeScreen() {
     <MapView
         style={{ flex: 1 }}
         mapType="hybrid"
-        camera={{
+        Camera={{
           center: {
-            latitude: 65.010,
-            longitude: 25.4719,
+            latitude: location.latitude,
+            longitude: location.longitude,
           },
           pitch: 90, 
           heading: 0,
@@ -86,6 +132,11 @@ export default function HomeScreen() {
         showsCompass={false}
         showsBuildings={true}
         pitchEnabled={false}
+        showsMyLocationButton={true}
+        onLongPress={handleLongPress}
+        zoomEnabled={true}
+        zoomControlEnabled={false}
+        scrollEnabled={true}
       >
 
 <Marker  coordinate={{
@@ -97,15 +148,40 @@ export default function HomeScreen() {
 <Image source={require('../images/marker.png')} style={{height: 40, width: 40 }} />
 
 </Marker>
+{markers.map((marker) => (
+          <Marker
+            key={marker.key}
+            coordinate={marker.coordinate}
+          />
+        ))}
+<Polyline
+    coordinates={[
+      {latitude: 65.06254, longitude: 25.46997},
+      {latitude: 65.06293, longitude: 25.46756},
+      {latitude: 65.06464, longitude: 25.46799},
+      {latitude: 65.06462, longitude: 25.47168},
+      {latitude: 65.06424, longitude: 25.47498},
+      {latitude: 65.06282, longitude: 25.47494},
+    ]}
+    strokeColor="red" 
+    strokeWidth={4}
+  />
+
 </MapView>
-<TextInput
-      style={styles.searchBar}
-      placeholder="Hae..."
-      value={search}
-      onChangeText={handleSearchChange}
-    />
+<Button title="Show Options" onPress={toggleAppOptions} />
+{showAppOptions && (
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Button title="Reset" onPress={onReset} />
+            <Button title="Reitti" onPress={Routefinder} />
+          </View>
+        </View>
+      )}
+          
+   
     
-    <Text style={styles.content}>Hakutulokset</Text>
+    
+
   </SafeAreaView>
 );
 }
@@ -130,5 +206,14 @@ content: {
 }, map: {
   height: '100%',
   width: '100%'
-}
+},
+routeContainer: {
+  position: 'absolute',
+  height: 40,
+  bottom: 40,
+},
+optionsRow: {
+  alignItems: 'center',
+  flexDirection: 'row',
+},
 });
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,83 +1,237 @@
-import { View, Text, StyleSheet, Button, TextInput} from 'react-native';
-import { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Modal } from "react-native";
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { Colors } from "../theme/colors";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const RegisterScreen = ({ navigation }) => {
+  const { register } = useAuth();
+  const [email, setEmail] = useState("");
+  /* const [username, setUsername] = useState(""); */
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+  // Register function
+  const handleRegister = async () => {
+    if (password !== confirmPassword) {
+      setErrorMessage("Salasanat eivät täsmää");
+      setModalVisible(true);
+      return;
+    }
+    try {
+      await register(email, password);
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        setErrorMessage("Sähköposti on jo käytössä");
+        setModalVisible(true);
+      } else if (error.code === "auth/invalid-email") {
+        setErrorMessage("Sähköposti ei kelvollinen");
+        setModalVisible(true);
+      } else if (error.code === "auth/weak-password") {
+        setErrorMessage("Salasanan tulee olla vähintään 6 merkkiä");
+        setModalVisible(true);
+      }
+    }
+  };
 
-    const handleRegister = () => {
-        if (password !== confirmPassword) {
-          alert("Salasanat eivät täsmää");
-          return;
-        }
-        console.log("Email:", email);
-        console.log("Username:", username);
-        console.log("Password:", password);
-      };
-    
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Luo uusi tili</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Sähköposti"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Käyttäjänimi"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Salasana"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Vahvista salasana"
-        value={confirmPassword}
-        onChangeText={setConfirmPassword}
-        secureTextEntry
-      />
-      <Button title="Rekisteröidy" onPress={handleRegister} />
-      <Text style={styles.footer}>Onko sinulla jo tili?</Text>
-      <Button title="Kirjaudu sisään" onPress={() => navigation.navigate('Login')} />
+      <View style={styles.topBackground}>
+        <Text style={styles.title}>Rekisteröidy</Text>
+      </View>
+
+      <View style={styles.bottomBackground} />
+
+      <View style={styles.contentContainer}>
+        <View style={styles.inputContainer}>
+          <MaterialIcons name="email" size={20} color={Colors.secondary} />
+          <TextInput
+            style={styles.input}
+            placeholder="Sähköposti"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+          />
+        </View>
+        {/* <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Käyttäjänimi"
+            value={username}
+            onChangeText={setUsername}
+          />
+        </View> */}
+        <View style={styles.inputContainer}>
+          <MaterialIcons name="lock" size={20} color={Colors.secondary} />
+          <TextInput
+            style={styles.input}
+            placeholder="Salasana"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+        </View>
+        <Text style={styles.passwordText}>
+          Salasanan tulee olla vähintään 6 merkkiä pitkä
+        </Text>
+        <View style={styles.inputContainer}>
+          <MaterialIcons name="lock" size={20} color={Colors.secondary} />
+          <TextInput
+            style={styles.input}
+            placeholder="Vahvista salasana"
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry
+          />
+        </View>
+
+        <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
+          <Text style={styles.registerButtonText}>Rekisteröidy</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.loginText}>Onko sinulla jo tili?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+          <Text style={styles.loginLink}>Kirjaudu</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Modal for error messages */}
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>{errorMessage}</Text>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.modalButtonText}>Ok</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 20,
-    },
-    header: {
-      fontSize: 24,
-      marginBottom: 20,
-    },
-    input: {
-      width: "100%",
-      padding: 10,
-      marginVertical: 10,
-      borderWidth: 1,
-      borderColor: "#ccc",
-      borderRadius: 5,
-    },
-    footer: {
-      marginTop: 20,
-      color: "blue",
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+  topBackground: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.onPrimaryFixed,
+  },
+  bottomBackground: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    alignItems: "center",
+  },
+  contentContainer: {
+    position: "absolute",
+    top: "40%",
+    alignSelf: "center",
+    alignItems: "center",
+    width: "90%",
+    backgroundColor: Colors.onPrimary,
+    padding: 20,
+    borderRadius: 20,
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  title: {
+    fontSize: 40,
+    color: "white",
+    width: "70%",
+    textAlign: "center",
+    fontFamily: "Exo_400Regular",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.secondaryContainer,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginTop: 20,
+    marginBottom: 5,
+  },
+  input: {
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 16,
+    color: Colors.secondary,
+  },
+  passwordText: {
+    fontSize: 12,
+    width: "100%",
+  },
+  registerButton: {
+    width: 190,
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 40,
+    borderWidth: 0,
+    backgroundColor: Colors.onPrimaryFixed,
+    margin: 20,
+  },
+  registerButtonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  loginText: {
+    marginTop: 50,
+    color: Colors.onPrimaryContainer,
+    fontSize: 14,
+  },
+  loginLink: {
+    fontWeight: "bold",
+    color: Colors.onPrimaryContainer,
+    fontSize: 16,
+    textDecorationLine: "underline",
+  },
+  /* Modal styles */
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalContent: {
+    width: 300,
+    backgroundColor: Colors.onPrimary,
+    padding: 20,
+    borderRadius: 20,
+    alignItems: "center",
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 20,
+    color: Colors.onPrimaryContainer
+  },
+  modalButton: {
+    padding: 10,
+    backgroundColor: Colors.onPrimaryFixed,
+    borderRadius: 40,
+    margin: 5,
+    width: "40%",
+    alignItems: "center",
+  },
+  modalButtonText: {
+    color: "white",
+    fontSize: 16,
+  },
+});
 
 export default RegisterScreen;
