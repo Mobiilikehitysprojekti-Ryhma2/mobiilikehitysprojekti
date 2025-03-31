71f7dc2b8fc997e1235223a06de281ef9edd1af5
t3roja <ronkainen@hotmail.fi>
2025-03-31 19:34:08 +0300

    Profile screen UI

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,91 +1,118 @@
 import React, { useState } from "react";
-import { View, Text, Image, Button, StyleSheet } from "react-native";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
+import { Colors } from "../theme/colors";
+import Button from "../components/Button";
 
 export default function ProfileScreen({ navigation }) {
-    const [user, setUser] = useState({
-        username: "käyttäjä1",
-        fullName: "Etunimi Sukunimi",
-        country: "Suomi",
-        bio: "Moi",
-        profilePic: "https://via.placeholder.com/150"
-      });
+  const [user, setUser] = useState({
+    username: "Käyttäjänimi",
+    fullName: "Etunimi Sukunimi",
+    country: "Suomi",
+    bio: "Autem voluptatem atque maiores dignissimos dolorem rerum doloremque.Dolorem sint repudiandae sunt ad voluptatibus.",
+    profileImage: require("../assets/placeHolderProfileImage.jpg")
+  });
 
   return (
     <View style={styles.container}>
-<View style={styles.header}>
-        <Text style={styles.title}>Profiili</Text>
+
+      <View style={styles.goBackButton}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons name="chevron-back-outline" size={42} color={Colors.primary} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.profileToolBar}>
+
         <Ionicons
           name="settings"
-          size={30}
-          color="black"
+          style={styles.toolbarIcon}
+          size={40}
+          color={Colors.primary}
           onPress={() => navigation.navigate('Settings')}
         />
-      </View>
-      <View style={styles.profileCard}>
-        <Image
-          source={{ uri: 'https://example.com/profile.jpg' }}
-          style={styles.profileImage}
+        <Ionicons
+          name="notifications-outline"
+          size={40}
+          color={Colors.primary}
+          onPress={() => navigation.navigate('Settings')}
         />
-        <Text style={styles.username}>Käyttäjätunnus</Text>
-        <Text style={styles.name}>Etunimi Sukunimi</Text>
-        <Text style={styles.bio}>Biografia: Moi</Text>
-        <Text style={styles.country}>Maa: Suomi</Text>
+
       </View>
+
+      <Image
+        source={user.profileImage}
+        style={styles.profileImage}
+      />
+      <Text style={styles.username}>{user.username}</Text>
+
+
+      <View style={styles.contentContainer}>
+
+        <Text style={styles.userInfo}>{user.fullName}</Text>
+        <Text style={styles.userInfo}>{user.country}</Text>
+        <Text style={styles.userInfo}>{user.bio}</Text>
+
+        <Button title="Lisää kaveriksi" styleType="primary" />{/* TODO: add add to friend functionality */}
+
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 20,
-      backgroundColor: '#f2f2f2',
-    },
-    header: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-    },
-    profileCard: {
-      marginTop: 20,
-      padding: 20,
-      backgroundColor: 'white',
-      borderRadius: 10,
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 4 },
-      shadowOpacity: 0.3,
-      shadowRadius: 4,
-      elevation: 5,
-      alignItems: 'center',
-      backgroundColor:"#006A66"
-    },
-    profileImage: {
-      width: 100,
-      height: 100,
-      borderRadius: 50,
-      marginBottom: 10,
-    },
-    username: {
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    name: {
-      fontSize: 16,
-      color: 'white',
-    },
-    bio: {
-      fontSize: 14,
-      textAlign: 'center',
-      marginVertical: 10,
-      color:"white"
-    },
-    country: {
-      fontSize: 14,
-      color: 'white',
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: Colors.background,
+  },
+  contentContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: "50%",
+    backgroundColor: Colors.onPrimaryFixed,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profileImage: {
+    position: "absolute",
+    top: 140,
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: Colors.primary,
+    zIndex: 10
+  },
+  userInfo: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginVertical: 10,
+    color: "white"
+  },
+  username: {
+    fontSize: 40,
+    color: Colors.primary,
+    marginTop: 360,
+    textAlign: "center",
+
+  },
+  profileToolBar: {
+    position: "absolute",
+    right: 18,
+    top: 60,
+  },
+  toolbarIcon: {
+    paddingBottom: 20,
+  },
+  goBackButton: {
+    position: "absolute",
+    top: 60,
+    left: 18,
+  }
+});
